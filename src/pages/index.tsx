874b65026f1da2c796ad793dd5b367c0d7ca05ec
kelvyn <kelvyn2reis@gmail.com>
2021-05-19 21:28:11 -0300

    executando build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
 
 
-export default function Home() {
+export default function Home(props) {
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+    
   )
+}
+
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return{
+    props:{
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
